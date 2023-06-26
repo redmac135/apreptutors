6f25eadaa8aee603b822660db7b2506c86c102f4
redmac135 <ethanzhao1981@gmail.com>
2023-06-26 17:30:54 -0400

    rought homepage outline

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="intro">
+		<div class="left-side">
+			<h1>aPrep Tutoring</h1>
+			<p>slogan</p>
+			<p>HL SUBJECTS...</p>
+		</div>
+		<div class="right-side">image</div>
+	</div>
+	<div class="button-wrapper">
+		<button type="button">Register</button>
+	</div>
+</main>
+
+<style>
+	.intro {
+		display: flex;
+		justify-content: center;
+	}
+
+	.left-side {
+		margin: 4rem;
+	}
+
+	.right-side {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		/* PLACEHOLDER VALUES FOR IMAGE */
+		width: 200px;
+		height: 200px;
+	}
+
+	.button-wrapper {
+		display: flex;
+		justify-content: center;
+	}
+</style>
