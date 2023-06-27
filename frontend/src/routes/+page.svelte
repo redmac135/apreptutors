039f7efd884b548b733f23ffa54a0be37bc88ce4
Bryan Deng <bryan.deng002@gmail.com>
2023-06-26 21:55:17 -0400

    layout structure for homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,71 @@
 <main>
-	<div class="intro">
+	<div class="section slogan-container">
+		<h1 class="slogan">SLOGAN</h1>
+		<!-- TODO ANCHORs -->
+		<div><a href="/#">Learn More</a></div>
+	</div>
+
+	<div class="section flex-container">
 		<div class="left-side">
-			<h1>aPrep Tutoring</h1>
-			<p class="description">
-				small description goes here Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-				eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-				nostrud exercita
+			<h1 class="title-text">Who we are</h1>
+			<p class="description-text">
+				mission statement text mission statement text mission statement text mission statement text
+				mission statement text mission statement text mission statement text mission statement text
+				mission statement text mission statement text mission statement text mission statement text
+				mission statement text mission statement text mission statement text
 			</p>
-			<div class="subjects">
-                <p>HL: subjects subjects subjects</p>
-                <p>SL: subjects subjects subjects</p>
-            </div>
-            <button type="button">Register</button>
 		</div>
 		<div class="right-side">image</div>
+	</div>
+
+	<div class="section">
+		<h1 class="title-text">Background</h1>
+		<p class="description-text">
+			background text background text background text background text background text background
+			text background text background text background text background text background text
+			background text background text background text background text background text background
+			text background text background text background text background text background text
+			background text background text background text
+		</p>
+	</div>
+
+	<div class="section">
+		<h1 class="title-text">What we offer</h1>
+		<p>HL SUBJECT, HL SUBJECT, HL SUBJECT, HL SUBJECT, HL SUBJECT</p>
+		<p>SL SUBJECT, SL SUBJECT, SL SUBJECT, SL SUBJECT, SL SUBJECT</p>
+	</div>
+
+	<div class="section">
+		<h1 class="title-text">Pricing</h1>
+		<div class="packages">
+			<div class="pricing-package">
+				<h2>Package 1</h2>
+				<p>
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text
+				</p>
+			</div>
+			<div class="pricing-package">
+				<h2>Package 2</h2>
+				<p>
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text
+				</p>
+			</div>
+			<div class="pricing-package">
+				<h2>Package 3</h2>
+				<p>
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text pricing text pricing text pricing text pricing text pricing text pricing text
+					pricing text
+				</p>
+			</div>
+		</div>
 	</div>
 </main>
 
@@ -23,28 +75,37 @@
 		max-width: 75%;
 	}
 
-	.intro {
+	.section {
+		margin: 10rem 0;
+	}
+
+	.slogan-container {
 		display: flex;
-		justify-content: space-evenly;
-        margin: 6rem 0;
+		flex-direction: column;
+		align-items: center;
 	}
 
-    .left-side {
-        width: 50%;
-    }
+	.slogan {
+		font-size: 6rem;
+	}
 
-	.left-side > h1 {
-		padding: 0.5rem;
+	.flex-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.left-side {
+		width: 50%;
+	}
+
+	.title-text {
 		font: x-large;
-		color: var(--white);
-		background-color: var(--dark-blue);
-		transform: skew(-5deg);
-		transform-origin: top;
+		margin: 1rem 0;
 	}
 
-    .description {
-        margin: 0.5rem 0;
-    }
+	.description-text {
+		margin: 1rem 0;
+	}
 
 	.right-side {
 		display: flex;
@@ -53,11 +114,18 @@
 		width: 30%;
 	}
 
-	button {
-		font-size: large;
-        color: var(--white);
-		padding: 0.7rem;
-		background-color: var(--light-blue);
-		border-radius: 0.5rem;
+	.packages {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.pricing-package {
+		padding: 1rem;
+		border: 1px solid black;
+		margin: 0 0.5rem;
+	}
+
+	.pricing-package > h2 {
+		text-align: center;
 	}
 </style>
