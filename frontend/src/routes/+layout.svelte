5cda87c3c2bed94473440d177bfde8af2f2e2b96
redmac135 <ethanzhao1981@gmail.com>
2023-06-26 23:53:21 -0400

    add infobar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Navbar from '../lib/Navbar.svelte';
+	import InfoBar from '../lib/InfoBar.svelte';
 
 	import '../app.css';
 </script>
 
 <Navbar />
+<InfoBar />
 <slot />
