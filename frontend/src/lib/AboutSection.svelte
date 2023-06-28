1013c15948f97953f91f8f4984080a7003cb2330
redmac135 <ethanzhao1981@gmail.com>
2023-06-27 23:36:32 -0400

    add max width to about section

--- frontend/src/lib/AboutSection.svelte
+++ frontend/src/lib/AboutSection.svelte
@@ -1,25 +1,27 @@
 <div class="container">
-	<div class="text-container">
-		<h1 class="title">Our mission</h1>
-		<p class="description">
-			Preparing future IB students to excel in examinations with curated lessons, networking with
-			recent alumni, and a vast databank of resources specifically for IB students.
-		</p>
-	</div>
-	<div class="info-container">
-		<div class="info">
-			<h2>IB</h2>
-			<p>graduates</p>
+	<div class="content">
+		<div class="text-container">
+			<h1 class="title">Our mission</h1>
+			<p class="description">
+				Preparing future IB students to excel in examinations with curated lessons, networking with
+				recent alumni, and a vast databank of resources specifically for IB students.
+			</p>
 		</div>
-		<div class="right-sep" />
-		<div class="info">
-			<h2>7s</h2>
-			<p>in all courses</p>
-		</div>
-		<div class="right-sep" />
-		<div class="info">
-			<h2>97+</h2>
-			<p>final mark averages</p>
+		<div class="info-container">
+			<div class="info">
+				<h2>IB</h2>
+				<p>graduates</p>
+			</div>
+			<div class="right-sep" />
+			<div class="info">
+				<h2>7s</h2>
+				<p>in all courses</p>
+			</div>
+			<div class="right-sep" />
+			<div class="info">
+				<h2>97+</h2>
+				<p>final mark averages</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -28,6 +30,13 @@
 	.container {
 		background-color: var(--light-blue);
 		padding: 6rem;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
+
+	.content {
+		max-width: 60rem;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
@@ -73,7 +82,7 @@
 	}
 
 	@media (min-width: 1000px) {
-		.container {
+		.content {
 			flex-direction: row;
 		}
 
