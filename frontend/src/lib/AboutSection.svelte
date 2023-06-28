3ea338ea8b1869bdca2786daa1178b410b264778
Bryan Deng <bryan.deng002@gmail.com>
2023-06-27 22:13:18 -0400

    improve about section

--- frontend/src/lib/AboutSection.svelte
+++ frontend/src/lib/AboutSection.svelte
@@ -1,50 +1,65 @@
-<div class="section" id="aboutus">
-	<div class="container">
-		<div class="left">
-			<h1 class="title"><span>For IB students,</span><span>by IB students</span></h1>
-			<p class="description">
-				We are a group of ib students text text text text text text text text text text text text
-				text text text text text text text text text text text text text text text text text text
-			</p>
+<div class="container">
+	<h1 class="title">Our mission</h1>
+	<p class="description">
+		Preparing future IB students to excel in examinations with curated lessons, networking with
+		recent alumni, and a vast databank of resources specifically for IB students.
+	</p>
+	<div class="info-container">
+		<div class="info">
+			<h2>IB</h2>
+			<p>graduates</p>
 		</div>
-		<div class="right">image!</div>
+		<div class="right-sep" />
+		<div class="info">
+			<h2>7s</h2>
+			<p>in all courses</p>
+		</div>
+		<div class="right-sep" />
+		<div class="info">
+			<h2>97+</h2>
+			<p>final mark averages</p>
+		</div>
 	</div>
 </div>
 
 <style>
-	.section {
-		background-color: var(--light-blue);
-        padding: 4rem 0;
-	}
-
 	.container {
-		width: 75%;
-		margin: auto;
-		padding: 4rem;
+		background-color: var(--light-blue);
+		padding: 6rem;
 		display: flex;
-        justify-content: space-between;
+		flex-direction: column;
+		align-items: center;
 	}
 
-    .title {
-        font-size: 3rem;
-        margin: 1rem 0;
-    }
-
-    .title span {
-        display: block;
-    }
-
-    .description {
-        font-size: 1.5rem;
-        margin: 1rem 0;
-    }
-
-	.left {
-		width: 60%;
-		padding: 0 3rem;
+	.title {
+		font-size: 3rem;
+		margin-bottom: 1rem;
 	}
 
-	.right {
-		width: 30%;
+	.description {
+		font-size: 1.5rem;
+		width: 75%;
+		text-align: center;
+	}
+
+	.info-container {
+		display: flex;
+		justify-content: space-evenly;
+		margin-top: 3rem;
+		width: 75%;
+	}
+
+	.info {
+		text-align: center;
+		width: 20%;
+		margin: auto;
+	}
+
+	.right-sep {
+		border-right: 2px solid var(--dark-blue);
+	}
+
+	.info h2 {
+		font-size: 4rem;
 	}
 </style>
