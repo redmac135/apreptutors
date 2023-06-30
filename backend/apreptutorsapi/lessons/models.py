a7b300d7f4c6c5c2a2209c081c1b98b6a3c27287
redmac135 <ethanzhao1981@gmail.com>
2023-06-29 23:25:53 -0400

    complete models for lessons

--- backend/apreptutorsapi/lessons/models.py
+++ backend/apreptutorsapi/lessons/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -22,6 +23,15 @@ class Qualification(models.Model):
     def __str__(self) -> str:
         return self.type + " " + self.name
 
+    def check_qualified(self, instructor: Profile):
+        if not instructor.is_teacher:
+            raise PermissionError("Not Teacher")
+        return (
+            InstructorQualification.get_qualifications_queryset(instructor)
+            .filter(qualification=self)
+            .exists()
+        )
+
 
 class InstructorQualification(models.Model):
     instructor = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -29,6 +39,20 @@ class InstructorQualification(models.Model):
 
     def __str__(self) -> str:
         return str(self.instructor) + " " + str(self.qualification)
+
+    @classmethod
+    def create_instructorqualification(
+        cls, instructor: Profile, qualification: Qualification
+    ):
+        if not instructor.is_teacher:
+            raise PermissionError("Not Teacher")
+        return cls.objects.create(instructor=instructor, qualification=qualification)
+
+    @classmethod
+    def get_qualifications_queryset(cls, instructor: Profile):
+        if not instructor.is_teacher:
+            raise PermissionError("Not Teacher")
+        return cls.objects.filter(instructor=instructor)
 
 
 class Timeslot(models.Model):
@@ -48,14 +72,43 @@ class Timeslot(models.Model):
         (FRIDAY, "Friday"),
         (SATURDAY, "Saturday"),
     ]
+    ALLOWED_TIMES = [
+        (datetime.time(9, 0, 0), "9:00"),
+        (datetime.time(10, 30, 0), "10:30"),
+        (datetime.time(12, 0, 0), "12:00"),
+        (datetime.time(13, 30, 0), "13:30"),
+        (datetime.time(14, 0, 0), "14:00"),
+        (datetime.time(16, 30, 0), "16:30"),
+        (datetime.time(18, 0, 0), "18:00"),
+        (datetime.time(19, 30, 0), "19:30"),
+        (datetime.time(21, 0, 0), "21:00"),
+    ]
 
     is_available = models.BooleanField()
-    weekday = models.CharField(max_length=3)
-    time = models.TimeField()
+    weekday = models.CharField(max_length=3, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField(choices=ALLOWED_TIMES)
     instructor = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return str(self.time) + " " + self.weekday + " " + str(self.instructor)
+
+    @classmethod
+    def create_timeslot(
+        cls,
+        weekday: str,
+        time: datetime.time,
+        instructor: Profile,
+        is_available: bool = True,
+    ) -> None:
+        if not instructor.is_teacher:
+            raise PermissionError("Not Teacher")
+        else:
+            return cls.objects.create(
+                weekday=weekday,
+                time=time,
+                instructor=instructor,
+                is_available=is_available,
+            )
 
 
 class Lesson(models.Model):
@@ -65,3 +118,11 @@ class Lesson(models.Model):
 
     def __str__(self) -> str:
         return str(self.subject) + " " + str(self.timeslot) + " " + str(self.student)
+
+    @classmethod
+    def create_lesson(timeslot: Timeslot, student: Profile, subject: Qualification):
+        if not student.is_student:
+            raise PermissionError("Not Student")
+        instructor = timeslot.instructor
+        if not subject.check_qualified(instructor):
+            raise PermissionError("Not Qualified")
