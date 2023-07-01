ca7432d266c2a869c6f8450cea7587c69bb697fd
redmac135 <ethanzhao1981@gmail.com>
2023-06-30 22:05:07 -0400

    add essential api views

--- backend/apreptutorsapi/lessons/urls.py
+++ backend/apreptutorsapi/lessons/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("instructor/<int:pk>", InstructorAPI.as_view(), name="instructor-api"),
-    path("locations/", LocationsListAPI.as_view(), name="locations-api")
+    path("locations/", LocationsListAPI.as_view(), name="locations-api"),
+    path("timeslot/<int:pk>", TimeslotAPI.as_view(), name="timeslot-api"),
+    path("timeslots/<int:qualification_pk>", TimeslotsListAPI.as_view(), name="timeslots-api"),
+    path("qualifications/", QualificationsListAPI.as_view(), name="qualifications-api")
 ]
