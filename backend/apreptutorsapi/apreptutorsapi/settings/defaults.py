4908b10b097c2f9b381a30de70afc63eba98cb14
redmac135 <ethanzhao1981@gmail.com>
2023-06-30 23:32:47 -0400

    install cors headers middleware

--- backend/apreptutorsapi/apreptutorsapi/settings/defaults.py
+++ backend/apreptutorsapi/apreptutorsapi/settings/defaults.py
@@ -70,6 +70,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "corsheaders",
+
     "lessons",
     "auth_firebase.apps.AuthFirebaseConfig",
     "rest_framework",
@@ -78,6 +81,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
