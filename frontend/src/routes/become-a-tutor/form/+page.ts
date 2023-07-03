baf6aabdf7886a26add60244fe073eafcf6f6e9b
Bryan Deng <bryan.deng002@gmail.com>
2023-07-02 20:44:46 -0400

    move api functions to api.ts

--- frontend/src/routes/become-a-tutor/form/+page.ts
+++ frontend/src/routes/become-a-tutor/form/+page.ts
@@ -1,26 +1,4 @@
-import { apiUrl } from "$lib/api";
-
-type Subject = {
-    pk: number;
-    type: string;
-    name: string;
-}
-async function getSubjects(): Promise<Subject[]> {
-    const API_URL = apiUrl("/qualifications");
-    const response = await fetch(API_URL);
-    return await response.json();
-}
-
-type Location = {
-    pk: number;
-    name: string;
-    address: string;
-}
-async function getLocations(): Promise<Location[]> {
-    const API_URL = apiUrl("/locations");
-    const response = await fetch(API_URL);
-    return await response.json();
-}
+import { getLocations, getSubjects } from "$lib/api";
 
 export const load = async () => {
     const subjects = await getSubjects();
